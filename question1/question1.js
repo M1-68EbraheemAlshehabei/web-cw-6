b597af773fb856dc1b8dc387f6d1b51cc78a1309
M1-68EbraheemAlshehabei <76258901+M1-68EbraheemAlshehabei@users.noreply.github.com>
2021-01-03 22:05:10 +0300

    Done!

--- question1/question1.js
+++ question1/question1.js
@@ -1,1 +1,15 @@
 // 👇🏻 قم بحل السؤال الأول هنا
+let username = prompt(`ما هو أسمك؟`); 
+
+function sayHello(){
+    alert(`مرحبا!${username}`);
+}
+
+
+if (username== null){
+    alert(`مرحبا!${username}`);
+    
+}
+else {
+    sayHello(username);
+}
